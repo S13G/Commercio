7b84341eb8d2fba9c8a8f4ddeb7f4a214b67adef
s13g <ayflix0@gmail.com>
2022-10-21 05:38:27 +0100

    created cart and adding products, removing products functionalities

--- Commercio/urls.py
+++ Commercio/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls', namespace='shop')),
+    path('cart/', include('cart.urls', namespace='cart')),
 ]
 
 if settings.DEBUG:
