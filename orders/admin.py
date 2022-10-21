8715949d245cbf5a609cc356dd4caff181bc79c2
s13g <ayflix0@gmail.com>
2022-10-21 14:58:52 +0100

    added custom actions to admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,9 +1,36 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from .models import OrderItem, Order
 
 
 # Register your models here.
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = 'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')  # instance of HttpResponse..file be treated as csv
+    response['Content-Disposition'] = content_disposition  # indicates the http response contains an attached file
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+        return response
+    export_to_csv.short_description = 'Export to CSV'
+
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
@@ -12,6 +39,7 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    actions = [export_to_csv]
     list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created',
                     'updated']
     list_filter = ['paid', 'created', 'updated']
